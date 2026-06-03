f93ddcf190d0f4988c91a671576fabf94385a54a
esalvati <esalvati@gmail.com>
2026-06-03 16:45:12 -0300

    Add CORS headers to responses for improved API accessibility

--- service/app.py
+++ service/app.py
@@ -25,6 +25,13 @@ def close_db(exc):
     if db:
         db.close()
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
 def novo_dia(ts1, ts2):
     # Testa se é um novo dia
     import datetime
